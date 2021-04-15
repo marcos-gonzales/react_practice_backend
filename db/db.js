5700b1485dc27bd939da40ebbc5e479092e034a4
Marcos Gonzales <markymarrk@gmail.com>
2021-04-14 18:16:45 -0600

    setting up heroku environment

--- db/db.js
+++ db/db.js
@@ -1,15 +1,30 @@
-require('dotenv').config()
-const Sequelize = require('sequelize')
+require('dotenv').config();
+const Sequelize = require('sequelize');
+let sequelize;
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    dialect: 'mysql',
-    host: 'localhost',
-    define: { freezeTableName: true },
-  }
-)
+if (process.env.NODE_ENV === 'production') {
+  // for Heroku
+  sequelize = new Sequelize(
+    process.env.JAWSDB_DB,
+    process.env.JAWSDB_USERNAME,
+    process.env.JAWDB_PASSWORD,
+    {
+      host: process.env.JAWSDB_HOST,
+      dialect: 'mysql',
+      logging: true,
+    }
+  );
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      dialect: 'mysql',
+      host: 'localhost',
+      define: { freezeTableName: true },
+    }
+  );
+}
 
-module.exports = sequelize
+module.exports = sequelize;
