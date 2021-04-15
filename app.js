973995b80eb47b0f500d8853688b2e9dc74f9f76
Marcos Gonzales <markymarrk@gmail.com>
2021-04-15 17:13:16 -0600

    troubleshooting socket.io in production

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
 const session = require('express-session');
-app.use('*', cors());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(
